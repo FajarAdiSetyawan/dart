886f8bad372e911b2c8e99d09906647760c5c492
Fajar Adi Setyawan <46274388+FajarAdiSetyawan@users.noreply.github.com>
2023-06-22 11:30:00 +0700

    dart libaray

---
+++ dart_library/example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:dart_library/hello.dart';
+import 'package:dart_library/src/customer.dart';
+
+void main(List<String> args) {
+  print(sayHello('Fajar'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ fundamental/bin/operator_type_test.dart
@@ -0,0 +1,6 @@
+void main(){
+  var nilaiAwal = 80;
+  var nilaiAkhir = 80;
+
+  var
+}
